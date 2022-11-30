2b41f5bf4404c93084a3f63a8e5c7dcda725c3ff
theLemmonade <lemmonmail@icloud.com>
2022-11-30 15:29:38 -0500

    server.js routes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,14 +1,80 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const { userInfo } = require('os');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('develop/public'));
 
-const api = require('./routes/apiRoutes')
-const html = require('./routes/htmlRoutes')
+// GET route for index
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+);
 
-app.listen(PORT, function() {
-    console.log(`App listening on http://localhost:${PORT}`)
+// GET route for notes
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+);
+
+// GET route for db
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to get notes`);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.json(JSON.parse(data))
+      }
+    });
+  });
+// POST route
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    const {title, text,} = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedReviews = JSON.parse(data);
+                parsedReviews.push(newNote);
+                fs.writeFile(`./db/db.json`,
+                JSON.stringify(parsedReviews),
+                (writeErr) =>
+                writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated Notes!')
+                );
+            };
+        });
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting Note');
+    }
 });
+//DELETE route
+app.delete("/api/notes/:id", (req, res) => {
+    console.info(`${req.method} request received to delete a note`);
+    let db = JSON.parse(fs.readFileSync('./db/db.json'))
+    let deleteNotes = db.filter(item => item.id !== req.params.id);
+       fs.writeFileSync('./db/db.json', JSON.stringify(deleteNotes));
+       res.json(deleteNotes);
+    });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
--- Develop/routes/htmlRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-module.exports = functions (apps) {
-    app.get('/notes', function (req, res) {
-        res.sendFile(path.join(__dirname, ))
-    })
-};
