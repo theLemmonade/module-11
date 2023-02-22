6302f66fbf2ca22bcc7f79470ac6caf07c1ba817
theLemmonade <lemmonmail@icloud.com>
2023-02-22 14:41:30 -0500

    added api post route

--- server.js
+++ server.js
@@ -1,37 +1,72 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-
-const { v4: uuidv4 } = require('uuid');
-const { userInfo } = require('os');
+const { v4: uuidv4 } = require("uuid");
+const { userInfo } = require("os");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-
-// HTML get Routes
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+// HTML get routes
+app.get("/", (req, res) =>
+	res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+	res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// API get  Route
-app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to get notes`);
+// API get route
+app.get("/api/notes", (req, res) => {
+	console.info(`${req.method} request received to get notes`);
 
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        res.json(JSON.parse(data))
-      }
-    });
-  });
+	fs.readFile("./db/db.json", "utf8", (err, data) => {
+		if (err) {
+			console.error(err);
+		} else {
+			res.json(JSON.parse(data));
+		}
+	});
+});
+
+// API post route
+app.post("/api/notes", (req, res) => {
+	console.info(`${req.method} request received to add a note`);
+	const { title, text } = req.body;
+	if (title && text) {
+		const newNote = {
+			title,
+			text,
+			id: uuidv4(),
+		};
+		fs.readFile("./db/db.json", "utf8", (err, data) => {
+			if (err) {
+				console.error(err);
+			} else {
+				const parsedReviews = JSON.parse(data);
+				parsedReviews.push(newNote);
+				fs.writeFile(
+					`./db/db.json`,
+					JSON.stringify(parsedReviews),
+					(writeErr) =>
+						writeErr
+							? console.error(writeErr)
+							: console.info("Successfully updated Notes!")
+				);
+			}
+		});
+		const response = {
+			status: "success",
+			body: newNote,
+		};
+		console.log(response);
+		res.status(201).json(response);
+	} else {
+		res.status(500).json("Error in posting Note");
+	}
+});
