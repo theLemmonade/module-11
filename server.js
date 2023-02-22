dd4c795a4d3c180769bb70b55350fe050be670ed
theLemmonade <lemmonmail@icloud.com>
2023-02-22 14:39:23 -0500

    added get routes for / and /notes

--- server.js
+++ server.js
@@ -13,3 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// HTML get Routes
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
