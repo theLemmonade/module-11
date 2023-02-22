8aefb9026598f3f6a060324d9cbfc8e6e7573b8f
theLemmonade <lemmonmail@icloud.com>
2023-02-22 14:40:04 -0500

    added api get route

--- server.js
+++ server.js
@@ -22,3 +22,16 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// API get  Route
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to get notes`);
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.json(JSON.parse(data))
+      }
+    });
+  });
